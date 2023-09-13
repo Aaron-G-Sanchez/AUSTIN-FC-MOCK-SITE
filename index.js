333e567393dd17613c5eedbd4c75bcfe6301992f
Aaron Sanchez <saarong12@gmail.com>
2023-09-12 23:11:51 -0500

    Added Austin Logo and created the nav bar with options

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+//GLOBAL ATTRIBUTES//
 const root = document.querySelector('.root')
+
+const navBar = document.createElement('nav')
+navBar.setAttribute('class', 'meanu')
+
+const navBarOptions = ['The Club', 'Tickets', 'Shop']
+const navBarList = document.createElement('ul')
+navBarList.setAttribute('class', 'nav-container')
+
+root.appendChild(navBar)
+navBar.appendChild(navBarList)
+
+for (i = 0; i < navBarOptions.length; i++) {
+  let navBarItem = document.createElement('li')
+  navBarItem.innerHTML = navBarOptions[i]
+  navBarList.appendChild(navBarItem)
+}
